7bf950024dc134d13c4310aac824b8205b2465b5
psmadbec <psmadbec@broadinstitute.org>
2024-10-29 14:32:15 -0400

    Add in temp files and metadata

--- src/ldsc/regression/main.py
+++ src/ldsc/regression/main.py
@@ -1,3 +1,4 @@
+import argparse
 import glob
 import json
 import numpy as np
@@ -8,44 +9,56 @@ from typing import Dict, List
 import inputs, ldsc, sumstats, weights, xtx_xty
 
 MAX_BLOCKS = 200
-data_path = os.environ.get('DATA_PATH', '.')
+input_path = os.environ['INPUT_PATH']
 
 
 def get_all_tissues(ancestry: str) -> List[str]:
     tissues = set()
-    for tissue_path in glob.glob(f'{data_path}/inputs/{ancestry}/tissue/tissue_ld.*___*.{ancestry}.npy'):
-        tissue = re.findall(f'{data_path}/inputs/{ancestry}/tissue/tissue_ld.(.*).{ancestry}.npy', tissue_path)[0]
+    for tissue_path in glob.glob(f'{input_path}/inputs/{ancestry}/tissue/tissue_ld.*___*.{ancestry}.npy'):
+        tissue = re.findall(f'{input_path}/inputs/{ancestry}/tissue/tissue_ld.(.*).{ancestry}.npy', tissue_path)[0]
         tissues |= {tissue}
     return sorted(tissues)
 
 
-def get_metadata():
-    with open('dataset/metadata', 'r') as f:
+def get_metadata(data_path: str) -> Dict:
+    with open(f'{data_path}/dataset/metadata', 'r') as f:
         metadata = json.load(f)
     return metadata
 
 
-def save_data(output: Dict) -> None:
-    os.makedirs(f'regression/', exist_ok=True)
+def get_version(ancestry):
+    with open(f'{input_path}/inputs/{ancestry}/version', 'r') as f:
+        return f.readline().strip()
+
+
+def save_data(output: Dict, metadata: Dict, data_path: str) -> None:
+    os.makedirs(f'{data_path}/output/', exist_ok=True)
     for variable_type, data in output.items():
-        file = f'regression/{variable_type}.output.tsv'
+        file = f'{data_path}/output/{variable_type}.output.tsv'
         with open(file, 'w') as f:
             for line in data:
                 f.write(line)
+    with open(f'{data_path}/output/metadata', 'w') as f:
+        metadata['version'] = get_version(metadata['ancestry'])
+        json.dump(metadata, f)
 
 
 def main():
-    metadata = get_metadata()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', default=None, required=True, type=str)
+    data_path = parser.parse_args().dir
+
+    metadata = get_metadata(data_path)
     ancestry = metadata['ancestry']
 
     chisq, sample_size, idxs = sumstats.load_sumstats(data_path)
     chisq, sample_size, idxs = sumstats.filter_sumstats(chisq, sample_size, idxs)
     mean_sample_size = float(np.mean(sample_size))
 
-    baseline_ld = inputs.get_baseline_ld(data_path, ancestry)
-    baseline_variables = inputs.get_baseline_variables(data_path, ancestry)
-    baseline_parameter_snps = inputs.get_baseline_parameter_snps(data_path, ancestry)
-    input_weights = weights.get_input_weights(data_path, ancestry)
+    baseline_ld = inputs.get_baseline_ld(input_path, ancestry)
+    baseline_variables = inputs.get_baseline_variables(input_path, ancestry)
+    baseline_parameter_snps = inputs.get_baseline_parameter_snps(input_path, ancestry)
+    input_weights = weights.get_input_weights(input_path, ancestry)
 
     sumstats_weights = input_weights[idxs]
     baseline_sumstats_ld = baseline_ld[idxs, :]
@@ -58,12 +71,12 @@ def main():
 
     output = {}
     for tissue in get_all_tissues(ancestry):
-        overlap_matrix = inputs.get_overlap(data_path, tissue, ancestry)
+        overlap_matrix = inputs.get_overlap(input_path, tissue, ancestry)
         total_snps = overlap_matrix[0][0]
 
-        variables = baseline_variables + inputs.get_tissue_variables(data_path, tissue, ancestry)
-        tissue_sumstats_ld = inputs.get_tissue_ld(data_path, tissue, ancestry)[idxs, :]
-        parameter_snps = np.vstack((baseline_parameter_snps, inputs.get_tissue_parameter_snps(data_path, tissue, ancestry)))
+        variables = baseline_variables + inputs.get_tissue_variables(input_path, tissue, ancestry)
+        tissue_sumstats_ld = inputs.get_tissue_ld(input_path, tissue, ancestry)[idxs, :]
+        parameter_snps = np.vstack((baseline_parameter_snps, inputs.get_tissue_parameter_snps(input_path, tissue, ancestry)))
 
         tissue_x = xtx_xty.get_x(tissue_sumstats_ld, baseline_weights, sample_size)
 
@@ -79,7 +92,7 @@ def main():
                 output[variable_type] = []
             line = f'{annotation}\t{tissue_name}\t{variable_name}\t{value["enrichment"]}\t{value["pValue"]}\n'
             output[variable_type].append(line)
-    save_data(output)
+    save_data(output, metadata, data_path)
 
 
 if __name__ == '__main__':
