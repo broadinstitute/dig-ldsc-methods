5b83fc7fea4b99061e3b0252a00458c55e147d73
Drew Hite <drew@drewhite.org>
2025-02-21 19:15:33 -0500

    fixes for uploads and downloads, also don't bundle the data in the dockerfile

--- src/main.py
+++ src/main.py
@@ -9,17 +9,17 @@ input_path = os.environ['INPUT_PATH']
 
 def download_raw(username: str, dataset: str, tmp_dir: str) -> None:
     path = f'{s3_bucket}/userdata/{username}/genetic/{dataset}/raw/'
-    subprocess.check_call(f'aws s3 cp {path} {tmp_dir}/raw/ --recursive')
+    subprocess.check_call(f'aws s3 cp "{path}" {tmp_dir}/raw/ --recursive', shell=True)
 
 
 def download_sumstats(username: str, dataset: str, tmp_dir: str) -> None:
     path = f'{s3_bucket}/userdata/{username}/genetic/{dataset}/sldsc/sumstats/'
-    subprocess.check_call(f'aws s3 cp {path} {tmp_dir}/sumstats/ --recursive')
+    subprocess.check_call(f'aws s3 cp "{path}" {tmp_dir}/sumstats/ --recursive', shell=True)
 
 
 def upload(username: str, dataset: str, tmp_dir: str, method: str) -> None:
     path = f'{s3_bucket}/userdata/{username}/genetic/{dataset}/sldsc/{method}'
-    subprocess.check_call(f'aws s3 cp {tmp_dir}/{method}/ {path} --recursive', shell=True)
+    subprocess.check_call(f'aws s3 cp {tmp_dir}/{method}/ "{path}" --recursive', shell=True)
 
 
 def main():
@@ -30,6 +30,7 @@ def main():
     args = parser.parse_args()
 
     tmp = tempfile.TemporaryDirectory()
+    print(f'Using temporary directory {tmp.name}')
     if args.method == 'sumstats':
         download_raw(args.username, args.dataset, tmp.name)
         subprocess.check_call(['python3.8', 'main.py', f'--dir={tmp.name}'], cwd='ldsc/sumstats/')
@@ -38,7 +39,6 @@ def main():
         download_sumstats(args.username, args.dataset, tmp.name)
         subprocess.check_call(['python3.8', 'main.py', f'--dir={tmp.name}'], cwd='ldsc/sldsc/')
         upload(args.username, args.dataset, tmp.name, args.method)
-    tmp.cleanup()
 
 
 if __name__ == '__main__':
