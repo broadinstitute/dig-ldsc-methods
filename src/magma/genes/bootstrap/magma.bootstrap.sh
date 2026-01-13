420e69f3f861f9c4108ea4f18ac0b07c13894789
psmadbec <psmadbec@broadinstitute.org>
2026-01-13 15:17:36 -0500

    hot fix: remove local only magma version

--- src/magma/genes/bootstrap/magma.bootstrap.sh
+++ src/magma/genes/bootstrap/magma.bootstrap.sh
@@ -7,6 +7,6 @@ fi
 DATA_DIR=$1
 INPUT_DIR=$2
 
-aws s3 cp ${DATA_DIR}/bin/magma/magma_v1.07bb_mac.zip ${INPUT_DIR}/inputs/
-unzip ${INPUT_DIR}/inputs/magma_v1.07bb_mac.zip -d ${INPUT_DIR}/inputs/magma
-rm ${INPUT_DIR}/inputs/magma_v1.07bb_mac.zip
+aws s3 cp ${DATA_DIR}/bin/magma/magma_v1.07bb_static.zip ${INPUT_DIR}/inputs/
+unzip ${INPUT_DIR}/inputs/magma_v1.07bb_static.zip -d ${INPUT_DIR}/inputs/magma
+rm ${INPUT_DIR}/inputs/magma_v1.07bb_static.zip
